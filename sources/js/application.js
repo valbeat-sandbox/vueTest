0ffc2678fbe00bc0a2b2a9b2cf7c4d636c287f7c
valbeat <kj1ktk@gmail.com>
2016-08-05 03:51:45 +0900

    create todo application

--- sources/js/application.js
+++ sources/js/application.js
@@ -1,16 +1,53 @@
 var vm = new Vue({
     el : '#app',
     data: {
-        title: 'testApp',
-        side: 20
+        tasks: [
+            { text: 'task 1', done: false},
+            { text: 'task 2', done: true},
+            { text: 'task 3', done: false}
+        ]
     },
-    computed: {
-        area: {
-            get: function() {
-                return this.side * this.side;
-            },
-            set: function (v) {
-                this.side = Math.sqrt(v);
+    computed : {
+        remaining: function () {
+            var count = 0;
+            for (var i = 0; i < this.tasks.length; i++) {
+                if (!this.tasks[i].done) {
+                    count++;
+                }
+            }
+            return count;
+        }
+    },
+    methods : {
+        add : function () {
+            if (this.newTask == '') return;
+            this.tasks.push({
+                text: this.newTask,
+                done: false
+            });
+            this.newTask = ''
+        },
+        delete : function (index) {
+            this.tasks.splice(index,1);
+        },
+        purge : function () {
+            var i = this.tasks.length;
+            while(i--) {
+                if (this.tasks[i].done) {
+                    this.tasks.splice(i,1);
+                }
+            }
+        },
+        selectAll: function () {
+            var i = this.tasks.length;
+            while(i--) {
+                this.tasks[i].done = true;
+            }
+        },
+        clearAll: function () {
+            var i = this.tasks.length;
+            while(i--) {
+                this.tasks[i].done = false;
             }
         }
     }
